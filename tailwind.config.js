dec2c37bb262b9384749ed732fafb46da93cd75e
Khoirony Arief <khoironyarief08@gmail.com>
2022-11-15 16:14:30 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/index.html',
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          xl: '1170px',
+        }
+      },
+      colors: {
+        grey: '#656464',
+        lightgrey: '#8C8C8C',
+        darkblue: '#0A093D',
+        darkblue2: '#2F2C4A',
+        blue: '#217BF4',
+        dark: '#4A4A56',
+      },
+      fontFamily: {
+        'sans': ['Inter'],
+        'serif': ['Inter'],
+        'mono': ['Inter'],
+        'display': ['Inter'],
+        'body': ['Inter']
+      }
+    },
+  },
+  plugins: [],
+}
